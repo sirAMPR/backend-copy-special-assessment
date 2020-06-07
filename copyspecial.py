e607318b14f483c0f1a9907967cc93d2168e48e8
Drew Radcliff <sirampr@gmail.com>
2020-06-07 19:06:37 -0400

    add tozip functionality

--- copyspecial.py
+++ copyspecial.py
@@ -42,8 +42,7 @@ def main(args):
     """Main driver code for copyspecial."""
     # This snippet will help you get started with the argparse module.
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--todir', help='dest dir for special files')
+    parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
     parser.add_argument('from_dir', help='directories to copy', nargs='+')
     # TODO: add one more argument definition to parse the 'from_dir' argument
@@ -51,6 +50,7 @@ def main(args):
 
     from_dir = ns.from_dir
     todir = ns.todir
+    tozip = ns.tozip
 
     # TODO: you must write your own code to get the command line args.
     # Read the docs and examples for the argparse module about how to do this.
@@ -59,13 +59,17 @@ def main(args):
     # This is input data validation. If something is wrong (or missing) with
     # any required args, the general rule is to print a usage message and
     # exit(1).
-
-    for item in get_special_paths(from_dir):
-        if todir:
-            os.makedirs(todir, exist_ok=True)
-            shutil.copy(item, todir)
-        else:
-            print(item)
+    file_list = get_special_paths(from_dir)
+    if tozip:
+        print("zip -j " + tozip + " " + "".join(file_list))
+        subprocess.run(["zip", "-j", tozip] + file_list)
+    else:
+        for item in file_list:
+            if todir:
+                os.makedirs(todir, exist_ok=True)
+                shutil.copy(item, todir)
+            else:
+                print(item)
 
 
 if __name__ == "__main__":
